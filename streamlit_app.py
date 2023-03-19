929f9d74dddea5bb68bd1e62a6e389d4f19be0d6
aseh123 <50541948+aseh123@users.noreply.github.com>
2023-03-19 13:31:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 import bech32
 
+st.title("Bech32 Converter")
 
+string = st.text_input("Enter a string to convert:")
 
-def main():
-    hrp = "bc"
-    data = bytes("Hello, world!".encode())
-    witprog = bech32.convertbits(data, 8, 5)
-    
-    out = bech32.encode(hrp, witprog)
-    if st.button("Convert"):
-        try:
-            result = out
-            st.success(f"Bech32 format: {result}")
-        except Exception as e:
-            st.error(f"Error: {e}")
-    
-if __name__ == "__main__":
-    main()
+if st.button("Convert"):
+    try:
+        hrp = "bc" # Human-readable part for Bitcoin addresses
+        data = bytes(string.encode())
+        witprog = bech32.convertbits(data, 8, 5)
+        result = bech32.encode(hrp, witprog)
+        st.success(f"Bech32 format: {result}")
+    except Exception as e:
+        st.error(f"Error: {e}")
