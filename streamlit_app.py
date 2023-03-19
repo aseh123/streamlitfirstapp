ff0dc503421fe3114223ffb44d39dc0a9f5b2e2a
aseh123 <50541948+aseh123@users.noreply.github.com>
2023-03-19 09:55:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,34 @@
 import streamlit as st
 import requests
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
 
 # Define a title for your app
-st.title("Current Bitcoin Price")
+st.title("Live Bitcoin Price Chart")
 
-# Fetch the current price of BTC from the CoinGecko API
-response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+# Set up the Plotly figure
+fig = go.Figure()
+
+# Fetch the historical price data for the past month from the CoinGecko API
+end_date = datetime.now()
+start_date = end_date - timedelta(days=30)
+end_date_str = end_date.strftime("%d-%m-%Y")
+start_date_str = start_date.strftime("%d-%m-%Y")
+response = requests.get(f"https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from={start_date.timestamp()}&to={end_date.timestamp()}")
 data = response.json()
-btc_price = data["bitcoin"]["usd"]
+prices = data["prices"]
+x_values = [datetime.fromtimestamp(price[0]/1000.0) for price in prices]
+y_values = [price[1] for price in prices]
+fig.add_trace(go.Scatter(x=x_values, y=y_values, name="BTC Price"))
 
-# Display the current price of BTC
-st.write(f"The current price of Bitcoin is ${btc_price}")
+# Define the layout for the Plotly figure
+fig.update_layout(
+    title="Bitcoin Price (USD) - Past Month",
+    xaxis_title="Date",
+    yaxis_title="Price (USD)",
+    template="plotly_dark"
+)
+
+# Display the live line chart of BTC price
+st.plotly_chart(fig, use_container_width=True)
+
