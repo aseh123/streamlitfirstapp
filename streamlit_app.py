8a7a61292af1a769f9b5ca64ca96df09d1fd3448
aseh123 <50541948+aseh123@users.noreply.github.com>
2023-03-19 09:43:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,10 @@
 import streamlit as st
-import plotly.express as px
-import pandas as pd
-import requests
 
-# Get BTC price data from Coindesk API
-response = requests.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2022-03-01&end=2023-03-01')
-btc_data = response.json()['bpi']
-df = pd.DataFrame.from_dict(btc_data, orient='index', columns=['BTC Price'])
+# Define a title for your app
+st.title("Welcome to my Streamlit App")
 
-# Create line chart using Plotly Express
-fig = px.line(df, x=df.index, y='BTC Price', title='BTC Price Chart')
+# Add a slider widget for the user to select a value
+user_input = st.slider("Select a value", 0, 100, 50)
 
-# Display chart in Streamlit
-st.plotly_chart(fig)
+# Add a 
+st.write(f"You selected: {user_input}")
