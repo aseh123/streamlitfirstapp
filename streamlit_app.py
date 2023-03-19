54787e030664d7de8e40d035b211ca0f192bfe40
aseh123 <50541948+aseh123@users.noreply.github.com>
2023-03-19 13:18:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,17 @@ import streamlit as st
 import bech32
 
 
-def bech32_converter(string: str) -> str:
-    """Converts a given string to bech32 format."""
-    hrp = "thor" # Human-readable part for rune addresses
-    data = bytes(string.encode())
-    witprog = bech32.convertbits(data, 8, 5)
-    return bech32.encode(hrp, witprog)
 
 def main():
     st.title("Bech32 Converter")
-
     string = st.text_input("Enter a string to convert:")
-    
+    hrp = "thor" # Human-readable part for rune addresses
+    data = bytes(string.encode())
+    witprog = bech32.convertbits(data, 8, 5)
+    out = bech32.encode(hrp, witprog)
     if st.button("Convert"):
         try:
-            result = bech32_converter(string)
+            result = out
             st.success(f"Bech32 format: {result}")
         except Exception as e:
             st.error(f"Error: {e}")
