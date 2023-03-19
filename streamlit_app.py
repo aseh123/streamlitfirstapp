7ab24ca9464375239c72ece25cebcd11ba2ce0b6
aseh123 <50541948+aseh123@users.noreply.github.com>
2023-03-19 11:14:16 +0530

    Update streamlit_app.py
    Fhg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,53 @@
 import streamlit as st
-import bech32
+from typing import Tuple
 
-def bech32_converter(string: str) -> str:
-    """Converts a given string to bech32 format."""
-    hrp = "bc" # Human-readable part for Bitcoin addresses
-    data = bytes(string.encode())
-    return bech32.encode(hrp, bech32.convertbits(data, 8, 5))
+# Define Bech32 character set and separator
+BECH32_CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+BECH32_SEPARATOR = "1"
 
-def main():
-    st.title("Bech32 Converter")
+# Bech32 encoding algorithm
+def bech32_encode(hrp: str, data: bytes) -> str:
+    # Create checksum
+    values = [ord(x) >> 5 for x in data] + [0] * 6
+    for i in range(len(data)):
+        values[i] = (values[i] ^ ord(data[i])) & 31
+    checksum = bech32_polymod([ord(x) for x in hrp + BECH32_SEPARATOR] + values + [0]*6) ^ 1
+    # Convert data and checksum to Bech32 string
+    return hrp + BECH32_SEPARATOR + ''.join([BECH32_CHARSET[d] for d in values + [checksum]])
 
-    string = st.text_input("Enter a string to convert:")
-    
-    if st.button("Convert"):
-        try:
-            result = bech32_converter(string)
-            st.success(f"Bech32 format: {result}")
-        except Exception as e:
-            st.error(f"Error: {e}")
-    
-if __name__ == "__main__":
-    main()
+def bech32_decode(bech: str) -> Tuple[str, bytes]:
+    if not all(x in BECH32_CHARSET for x in bech):
+        raise ValueError("Invalid characters in Bech32 string")
+    bech = bech.lower()
+    pos = bech.rfind(BECH32_SEPARATOR)
+    if pos == -1:
+        raise ValueError("Bech32 string missing separator")
+    if pos < 1 or pos + 7 > len(bech):
+        raise ValueError("Invalid Bech32 string length")
+    hrp = bech[:pos]
+    data = bytes([BECH32_CHARSET.find(x) for x in bech[pos+1:]])
+    if not bech32_verify_checksum(hrp, data):
+        raise ValueError("Invalid Bech32 checksum")
+    return hrp, data[:-6]
+
+def bech32_verify_checksum(hrp: str, data: bytes) -> bool:
+    values = [ord(x) & 31 for x in hrp] + [0] + [x for x in data]
+    return bech32_polymod(values) == 1
+
+def bech32_polymod(values: list) -> int:
+    generator = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
+    chk = 1
+    for v in values:
+        b = (chk >> 25) & 1
+        chk = ((chk & 0x1ffffff) << 5) ^ v
+        for i in range(5):
+            chk ^= generator[i] if ((b >> i) & 1) else 0
+    return chk
+
+# User input prompt
+input_bech32 = st.text_input("Enter a Bech32 string:", value="", max_chars=None, key=None, type='default')
+
+if input_bech32 != "":
+    try:
+        hrp, data = bech32_decode(input_bech32)
+       
