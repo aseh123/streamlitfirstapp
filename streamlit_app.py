e189fed5aa0a6595cdf4263a22ce35862f6df908
aseh123 <50541948+aseh123@users.noreply.github.com>
2023-04-28 12:19:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,66 +53,65 @@ if maya_address:
                 st.markdown(f"Your profit at the start of LA is ${round(profit_before_la, 2)}")
 
                 pred_rune = st.number_input("Predicted $RUNE Value:", value=3.00, min_value=0.0, step=0.01)
-				pred_cacao = st.number_input("Predicted $CACAO Value:", value=0.14173, min_value=0.0, step=0.00001)
-				pred_apy = st.number_input("Predicted number of days to withdraw:", value=50, min_value=50, step=1)
-				            if pred_rune and pred_cacao and pred_apy:
-                K = runefull * cacaofull
-                rt = pred_rune / pred_cacao
-                qp_rune = math.sqrt(K / rt)
-                qp_cacao = math.sqrt(K * rt)
-                p_value = qp_rune * pred_rune + qp_cacao * pred_cacao
-                hold_total_asset_value = pred_rune * runefull + pred_cacao * cacaofull
-                i_loss = p_value - hold_total_asset_value
-                i_loss_percent = (hold_total_asset_value - p_value) * 100 / hold_total_asset_value
-                init_ratio = 1.65 / 0.14173
-                pred_ratio = pred_rune / pred_cacao
-
-                st.markdown(f"#### If you withdraw at the {pred_apy} day")
-                st.markdown(f"Num of $RUNE: {round(qp_rune, 2)}")
-                st.markdown(f"Num of $CACAO: {round(qp_cacao, 2)}")
-                st.markdown(f"Your value of assets in ($): {round(p_value, 2)}")
-                st.markdown(f"Impermanent Loss (IL) percentage: {round(i_loss_percent, 2)}")
-                st.markdown(f"Performance vs HODL in ($): {round(-(hold_total_asset_value - p_value), 2)}")
-                st.markdown(f"Performance vs added asset in LA in ($): {round(p_value - 1.65 * runefull, 2)} Free Money")
-
-                if pred_ratio < init_ratio and i_loss < 0:
-                    if pred_apy > 450:
-                        pred_days = 450
-                    else:
-                        pred_days = pred_apy
-                    days_covered = (pred_days - 50)
-                    ILP_covered = -i_loss * days_covered / 400
-
-                    st.markdown("This is $cacao overperformance case, considers 400 days for full ILP")
-                    st.markdown(f"ILP covered in ($): {round(ILP_covered, 2)}")
-                    IlP_percent = -ILP_covered * 100 / i_loss
-                    st.markdown(f"ILP covered in percentage: {round(IlP_percent, 2)}")
-                    cacao_ILP = ILP_covered / pred_cacao
-                    st.markdown(f"ILP is Paid via $cacao: {round(cacao_ILP, 2)}")
-                    st.markdown(f"After ILP covered, You will have {round(qp_rune, 2)} Rune")
-                    total_cacao = qp_cacao + cacao_ILP
-                    st.markdown(f"After ILP covered, You will have {round(total_cacao, 2)} Cacao")
-                    st.markdown("Note: All these are based on assumptions, such as no withdrawal/deposit of funds from the LP position, and do not consider earned fees. This is still a work in progress - Beta Stage.")
-
-                elif pred_ratio >= init_ratio and i_loss < 0:
-                    if pred_apy > 150:
-                        pred_days = 150
-                    else:
-                        pred_days = pred_apy
-                    days_covered = (pred_days - 50)
-                    ILP_covered = -i_loss * days_covered / 100
-                    st.markdown("This is $rune overperformance case, considers 100 days for full ILP")
-                    st.markdown(f"ILP covered in ($): {round(ILP_covered, 2)}")
-                    IlP_percent = -ILP_covered * 100 / i_loss
-                    st.markdown(f"ILP covered in percentage: {round(IlP_percent, 2)}")
-                    cacao_ILP = ILP_covered / pred_cacao
-                    st.markdown(f"ILP is Paid via $cacao: {round(cacao_ILP, 2)}")
-                    st.markdown(f"After ILP covered, You will have {round(qp_rune, 2)} Rune")
-                    total_cacao = qp_cacao + cacao_ILP
-                    st.markdown(f"After ILP covered, You will have {round(total_cacao, 2)} Cacao")
-                    st.markdown("Note: All these are based on assumptions, such as no withdrawal/deposit of funds from the LP position, and do not consider earned fees. This is still a work in progress - Beta Stage.")
-               else:
-                st.markdown("No ILP coverage is needed in this scenario.")
-                st.markdown("Note: All these are based on assumptions, such as no withdrawal/deposit of funds from the LP position, and do not consider earned fees. This is still a work in progress - Beta Stage.")
-
+	        pred_cacao = st.number_input("Predicted $CACAO Value:", value=0.14173, min_value=0.0, step=0.00001)
+		pred_apy = st.number_input("Predicted number of days to withdraw:", value=50, min_value=50, step=1)
+	        if pred_rune and pred_cacao and pred_apy:
+                        K = runefull * cacaofull
+                        rt = pred_rune / pred_cacao
+                        qp_rune = math.sqrt(K / rt)
+                        qp_cacao = math.sqrt(K * rt)
+                        p_value = qp_rune * pred_rune + qp_cacao * pred_cacao
+                        hold_total_asset_value = pred_rune * runefull + pred_cacao * cacaofull
+                        i_loss = p_value - hold_total_asset_value
+                        i_loss_percent = (hold_total_asset_value - p_value) * 100 / hold_total_asset_value
+                        init_ratio = 1.65 / 0.14173
+                        pred_ratio = pred_rune / pred_cacao
+        
+                        st.markdown(f"#### If you withdraw at the {pred_apy} day")
+                        st.markdown(f"Num of $RUNE: {round(qp_rune, 2)}")
+                        st.markdown(f"Num of $CACAO: {round(qp_cacao, 2)}")
+                        st.markdown(f"Your value of assets in ($): {round(p_value, 2)}")
+                        st.markdown(f"Impermanent Loss (IL) percentage: {round(i_loss_percent, 2)}")
+                        st.markdown(f"Performance vs HODL in ($): {round(-(hold_total_asset_value - p_value), 2)}")
+                        st.markdown(f"Performance vs added asset in LA in ($): {round(p_value - 1.65 * runefull, 2)} Free Money")
+        
+                        if pred_ratio < init_ratio and i_loss < 0:
+                            if pred_apy > 450:
+                                pred_days = 450
+                            else:
+                                pred_days = pred_apy
+                            days_covered = (pred_days - 50)
+                            ILP_covered = -i_loss * days_covered / 400
+        
+                            st.markdown("This is $cacao overperformance case, considers 400 days for full ILP")
+                            st.markdown(f"ILP covered in ($): {round(ILP_covered, 2)}")
+                            IlP_percent = -ILP_covered * 100 / i_loss
+                            st.markdown(f"ILP covered in percentage: {round(IlP_percent, 2)}")
+                            cacao_ILP = ILP_covered / pred_cacao
+                            st.markdown(f"ILP is Paid via $cacao: {round(cacao_ILP, 2)}")
+                            st.markdown(f"After ILP covered, You will have {round(qp_rune, 2)} Rune")
+                            total_cacao = qp_cacao + cacao_ILP
+                            st.markdown(f"After ILP covered, You will have {round(total_cacao, 2)} Cacao")
+                            st.markdown("Note: All these are based on assumptions, such as no withdrawal/deposit of funds from the LP position, and do not consider earned fees. This is still a work in progress - Beta Stage.")
+        
+                        elif pred_ratio >= init_ratio and i_loss < 0:
+                            if pred_apy > 150:
+                                pred_days = 150
+                            else:
+                            pred_days = pred_apy
+                            days_covered = (pred_days - 50)
+                            ILP_covered = -i_loss * days_covered / 100
+                            st.markdown("This is $rune overperformance case, considers 100 days for full ILP")
+                            st.markdown(f"ILP covered in ($): {round(ILP_covered, 2)}")
+                            IlP_percent = -ILP_covered * 100 / i_loss
+                            st.markdown(f"ILP covered in percentage: {round(IlP_percent, 2)}")
+                            cacao_ILP = ILP_covered / pred_cacao
+                            st.markdown(f"ILP is Paid via $cacao: {round(cacao_ILP, 2)}")
+                            st.markdown(f"After ILP covered, You will have {round(qp_rune, 2)} Rune")
+                            total_cacao = qp_cacao + cacao_ILP
+                            st.markdown(f"After ILP covered, You will have {round(total_cacao, 2)} Cacao")
+                            st.markdown("Note: All these are based on assumptions, such as no withdrawal/deposit of funds from the LP position, and do not consider earned fees. This is still a work in progress - Beta Stage.")
+                        else:
+                        st.markdown("No ILP coverage is needed in this scenario.")
+                        st.markdown("Note: All these are based on assumptions, such as no withdrawal/deposit of funds from the LP position, and do not consider earned fees. This is still a work in progress - Beta Stage.")
 
